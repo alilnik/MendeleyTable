8f6fafe11412ed29aeb08afbc845f142bac43993
Ilya Dmitrenok <mjazz@list.ru>
2018-04-01 14:17:14 +0300

    Initial commit

---
+++ app/src/main/java/com/innopolis/mjazz/mendeleytable/ElementFragment.java
@@ -0,0 +1,23 @@
+package com.innopolis.mjazz.mendeleytable;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by mjazz on 11.03.2018.
+ */
+
+public class ElementFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_element_layout, container, false);
+    }
+
+
+}

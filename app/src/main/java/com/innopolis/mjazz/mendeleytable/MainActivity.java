ad19e83ea9db19f78c16882de9dd8bd325094bd7
Ilya Dmitrenok <mjazz@list.ru>
2018-04-22 13:30:35 +0300

    training with nik

--- app/src/main/java/com/innopolis/mjazz/mendeleytable/MainActivity.java
+++ app/src/main/java/com/innopolis/mjazz/mendeleytable/MainActivity.java
@@ -1,9 +1,10 @@
 package com.innopolis.mjazz.mendeleytable;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     ElementFragmentFactory factory;
 
